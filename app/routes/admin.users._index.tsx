0e4098097a9bba7dace40921bc21d45597c24dd1
Jacob Kwasi Bordes <kwasibordesjacob@gmail.com>
2024-02-25 09:26:30 +0000

    fix:: create admin form

--- app/routes/admin.users._index.tsx
+++ app/routes/admin.users._index.tsx
@@ -1,10 +1,28 @@
 import AdminLayout from "~/layouts/adminLayout";
 import CustomTable from "~/components/custom/CustomTable";
+import CustomInput from "~/components/custom/CustomInput";
 
 export default function Admins() {
   return (
     <AdminLayout>
-      <CustomTable />
+      <CustomTable
+        formItems={
+          <div className="flex flex-col gap-8">
+            <div className="grid grid-cols-2 gap-4">
+              <CustomInput label="First Name" name="firstName" />
+              <CustomInput label="Last Name" name="lastName" />
+            </div>
+            <div className="grid grid-cols-2 gap-4">
+              <CustomInput label="Phone" name="phone" />
+              <CustomInput label="Email" name="email" />
+            </div>
+            <div className="grid grid-cols-2 gap-4">
+              <CustomInput label="Password" name="password" />
+              <CustomInput label="Confirm Password" name="confirm" />
+            </div>
+          </div>
+        }
+      />
     </AdminLayout>
   );
 }
