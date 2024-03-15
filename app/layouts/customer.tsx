daca69afe3b8f4f36dd1bdf81803fbde2fadd0df
Jacob Kwasi Bordes <kwasibordesjacob@gmail.com>
2024-03-15 14:25:29 +0000

    desktop nav items completed

--- app/layouts/customer.tsx
+++ app/layouts/customer.tsx
@@ -1,6 +1,22 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { motion } from "framer-motion";
 import { ReactNode, useState, useRef, useEffect } from "react";
+import { NavLink, useLocation } from "@remix-run/react";
+import { ThemeSwitcher } from "~/components/ThemeSwitcher";
+import {
+  Dropdown,
+  DropdownTrigger,
+  User,
+  DropdownMenu,
+  DropdownItem,
+  Badge,
+  Button,
+  Accordion,
+  AccordionItem,
+} from "@nextui-org/react";
+
+// import data
+import { customerNavLinks } from "~/data/nav-links";
 
 const AppLogo = () => {
   return (
@@ -9,12 +25,12 @@ const AppLogo = () => {
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
         fill="currentColor"
-        className="size-6 text-blue-600"
+        className="size-8 text-blue-600"
       >
         <path d="M6 3a3 3 0 0 0-3 3v2.25a3 3 0 0 0 3 3h2.25a3 3 0 0 0 3-3V6a3 3 0 0 0-3-3H6ZM15.75 3a3 3 0 0 0-3 3v2.25a3 3 0 0 0 3 3H18a3 3 0 0 0 3-3V6a3 3 0 0 0-3-3h-2.25ZM6 12.75a3 3 0 0 0-3 3V18a3 3 0 0 0 3 3h2.25a3 3 0 0 0 3-3v-2.25a3 3 0 0 0-3-3H6ZM17.625 13.5a.75.75 0 0 0-1.5 0v2.625H13.5a.75.75 0 0 0 0 1.5h2.625v2.625a.75.75 0 0 0 1.5 0v-2.625h2.625a.75.75 0 0 0 0-1.5h-2.625V13.5Z" />
       </svg>
       <div>
-        <h1 className="text-sm font-extrabold font-montserrat bg-clip-text text-transparent bg-gradient-to-r from-violet-500 via-blue-500 to-violet-500">
+        <h1 className="text-sm font-extrabold font-montserrat bg-clip-text text-transparent bg-gradient-to-r from-violet-500 via-blue-500 to-violet-500 hidden lg:block">
           FarmFusion
         </h1>
       </div>
@@ -23,6 +39,7 @@ const AppLogo = () => {
 };
 
 const CustomerLayout = ({ children }: { children: ReactNode }) => {
+  const { pathname } = useLocation();
   const mobileNavRef = useRef(null);
   const handleOutsideNavClick = (e: any) => {
     if (!mobileNavRef.current.contains(e.target)) {
@@ -34,27 +51,98 @@ const CustomerLayout = ({ children }: { children: ReactNode }) => {
   }, []);
   const [menuVisible, setMenuVisible] = useState(false);
   return (
-    <main className="p-2 md:h-screen flex gap-4">
-      <aside className="hidden lg:flex h-full lg:w-[17%] rounded-xl dark:bg-slate-900"></aside>
+    <main className="p-2 h-screen flex gap-4 bg-blue-50 dark:bg-slate-950">
+      <aside className="hidden lg:flex h-full lg:w-[17%] rounded-xl bg-blue-600 dark:bg-slate-900 flex-col justify-between py-4">
+        <div></div>
+        <nav className="flex flex-col gap-2 px-3">
+          {customerNavLinks.map((navItem, index) => (
+            <NavLink
+              className={`${
+                navItem.href === pathname ? "bg-blue-500/50" : ""
+              } font-quicksand text-white rounded-lg py-2 flex items-center gap-2 hover:bg-blue-500/50 transition-all duration-300`}
+              key={index}
+              to={navItem.href}
+            >
+              {navItem.href === pathname ? (
+                <span className="w-1 h-full bg-white rounded-r-lg"></span>
+              ) : (
+                <span className="w-1"></span>
+              )}
+              {navItem.icon && navItem.icon}
+              {navItem.name}
+            </NavLink>
+          ))}
+        </nav>
+        <div></div>
+      </aside>
 
       <section className="h-full lg:flex-1 flex flex-col gap-4 w-full">
-        <header className="h-12 flex items-center justify-between bg-slate-900 rounded-xl px-3">
-          <AppLogo />
+        <header className="h-12 lg:h-14 flex items-center justify-between bg-white dark:bg-slate-900 rounded-xl px-3 lg:px-5">
+          <div className="flex items-center gap-1 md:gap-3">
+            <div className="block lg:hidden">
+              <AppLogo />
+            </div>
+            <h3 className="font-montserrat font-medium text-xl lg:text-3xl">
+              {
+                customerNavLinks.find((navItem) => pathname === navItem.href)
+                  ?.title
+              }
+            </h3>
+          </div>
 
-          <button
-            type="button"
-            onClick={() => setMenuVisible(true)}
-            className="lg:hidden outline-none border-none"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              className="size-6 text-slate-800 dark:text-white"
-              fill="currentColor"
+          <div className="flex items-center gap-2 md:gap-4 flex-row-reverse">
+            <button
+              type="button"
+              onClick={() => setMenuVisible(true)}
+              className="lg:hidden outline-none border-none"
             >
-              <path d="M3 4H21V6H3V4ZM3 11H21V13H3V11ZM3 18H21V20H3V18Z"></path>
-            </svg>
-          </button>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                className="size-8 text-slate-800 dark:text-white"
+                fill="currentColor"
+              >
+                <path d="M3 4H21V6H3V4ZM3 11H21V13H3V11ZM3 18H21V20H3V18Z"></path>
+              </svg>
+            </button>
+
+            {/* user avatar dropdown */}
+            <Dropdown placement="bottom-start">
+              <DropdownTrigger>
+                <User
+                  as="button"
+                  avatarProps={{
+                    isBordered: true,
+                    src: "https://i.pravatar.cc/150?u=a042581f4e29026024d",
+                    size: "sm",
+                    classNames: {
+                      base: "!size-6",
+                    },
+                  }}
+                  className="transition-transform"
+                  classNames={{
+                    name: "hidden md:flex",
+                    description: "hidden md:flex",
+                  }}
+                  description="Admin"
+                  name="Tony Reichert"
+                />
+              </DropdownTrigger>
+              <DropdownMenu aria-label="User Actions" variant="flat">
+                <DropdownItem key="profile" className="h-14 gap-2">
+                  <p className="font-bold">Signed in as</p>
+                  <p className="font-bold">@tonyreichert</p>
+                </DropdownItem>
+                <DropdownItem key="settings">My Settings</DropdownItem>
+                <DropdownItem key="logout" color="danger">
+                  Log Out
+                </DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+
+            {/* themeswitcher */}
+            <ThemeSwitcher />
+          </div>
 
           {/* mobile retractable sidebar */}
           <motion.aside
@@ -64,23 +152,17 @@ const CustomerLayout = ({ children }: { children: ReactNode }) => {
             variants={{
               hide: {
                 x: "-100%",
-                opacity: 0.5,
                 transition: {
                   all: {
                     type: "spring",
                     stiffness: 500,
                     damping: 30,
-                    duration: 1,
-                  },
-                  opacity: {
-                    easings: "ease",
                     duration: 1,
                   },
                 },
               },
               show: {
                 x: 0,
-                opacity: 1,
                 transition: {
                   all: {
                     type: "spring",
@@ -88,14 +170,10 @@ const CustomerLayout = ({ children }: { children: ReactNode }) => {
                     damping: 30,
                     duration: 1,
                   },
-                  opacity: {
-                    easings: "ease",
-                    duration: 1,
-                  },
                 },
               },
             }}
-            className="fixed top-0 left-0 bg-white dark:backdrop-blur-sm dark:bg-slate-900/30 h-full w-[90%] shadow-2xl border-r dark:border-slate-100/20 z-50"
+            className="fixed top-0 left-0 bg-white dark:backdrop-blur-lg dark:bg-slate-900/30 h-full w-[90%] md:w-[30%] shadow-2xl border-r dark:border-slate-100/20 z-50"
           >
             <div className="flex items-center justify-between px-3 py-2">
               <p>Menu</p>
@@ -107,87 +185,17 @@ const CustomerLayout = ({ children }: { children: ReactNode }) => {
                 X
               </button>
             </div>
-            {/* <nav className="flex flex-col gap-1">
-              {navbarLinks.map((link, index) => {
-                return (
-                  <div key={index}>
-                    {link.children.length > 0 ? (
-                      <Accordion
-                        motionProps={{
-                          variants: {
-                            enter: {
-                              y: 0,
-                              opacity: 1,
-                              height: "auto",
-                              transition: {
-                                height: {
-                                  type: "spring",
-                                  stiffness: 500,
-                                  damping: 30,
-                                  duration: 1,
-                                },
-                                opacity: {
-                                  easings: "ease",
-                                  duration: 1,
-                                },
-                              },
-                            },
-                            exit: {
-                              y: -10,
-                              opacity: 0,
-                              height: 0,
-                              transition: {
-                                height: {
-                                  easings: "ease",
-                                  duration: 0.25,
-                                },
-                                opacity: {
-                                  easings: "ease",
-                                  duration: 0.3,
-                                },
-                              },
-                            },
-                          },
-                        }}
-                        key={index}
-                        className="w-full"
-                      >
-                        <AccordionItem
-                          key={index}
-                          title={link.label}
-                          startContent={link.icon}
-                        >
-                          {link.children.map((child, index) => (
-                            <Link
-                              key={index}
-                              to={child.href}
-                              className="flex items-center gap-2 p-3 font-montserrat text-slate-800 dark:text-white hover:bg-slate-100 dark:hover:bg-slate-900/30"
-                            >
-                              {child.icon ? child.icon : ""}
-                              <span>{child.label}</span>
-                            </Link>
-                          ))}
-                        </AccordionItem>
-                      </Accordion>
-                    ) : (
-                      <Link
-                        to={link.href}
-                        className={`flex items-center gap-2 p-3 font-montserrat text-slate-800 dark:text-white hover:bg-slate-100 dark:hover:bg-slate-900/30`}
-                      >
-                        {link.icon ? link.icon : ""}
-                        <span>{link.label}</span>
-                      </Link>
-                    )}
-                  </div>
-                );
-              })}
-            </nav> */}
+            <nav className="flex flex-col gap-3">
+              {customerNavLinks.map((navItem, index) => (
+                <NavLink to={navItem.href} key={index}>
+                  {navItem.name}
+                </NavLink>
+              ))}
+            </nav>
           </motion.aside>
         </header>
 
-        <section className="flex-1 overflow-y-hidden dark:bg-slate-900 rounded-xl px-4 py-3">
-          {children}
-        </section>
+        <section className="flex-1 overflow-y-hidden">{children}</section>
       </section>
     </main>
   );
--- app/routes/customer.orders.tsx
+++ app/routes/customer.orders.tsx
@@ -1,5 +1,20 @@
-const CustomerDashboard = () => {
-  return <div>Dashboard</div>;
+import CustomerLayout from "~/layouts/customer";
+
+const CustomerOrders = () => {
+  return (
+    <CustomerLayout>
+      <div className="h-full flex flex-col gap-4 lg:gap-5">
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-4 md:gap-5">
+          <div className="rounded-xl px-3 py-2 h-16 bg-white dark:bg-slate-900"></div>
+          <div className="rounded-xl px-3 py-2 h-16 bg-white dark:bg-slate-900"></div>
+          <div className="rounded-xl px-3 py-2 h-16 bg-white dark:bg-slate-900"></div>
+          <div className="rounded-xl px-3 py-2 h-16 bg-white dark:bg-slate-900"></div>
+        </div>
+
+        <div className="flex-1 overflow-y-auto bg-white dark:bg-slate-900 rounded-xl"></div>
+      </div>
+    </CustomerLayout>
+  );
 };
 
-export default CustomerDashboard;
+export default CustomerOrders;
